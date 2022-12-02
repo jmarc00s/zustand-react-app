23d27f186ece34bb2a95e5fe5aeb985d72cacf1e
João Marcos <jmarcossferreira@hotmail.com>
2022-12-02 09:18:36 -0300

    Improvements on home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,16 @@ import React from 'react';
 export const HomePage = () => {
   return (
     <section>
-      <h1 className="text-3xl text-gray-800">Welcome to zustand react app!</h1>
+      <h1 className="text-3xl text-gray-800 text-center mb-4">
+        Welcome to zustand react app!
+      </h1>
+      <h2 className="text-gray-400 font-medium text-center">
+        This is an app to play around with the{' '}
+        <a target="_blank" href="https://github.com/pmndrs/zustand">
+          Zustand
+        </a>{' '}
+        state managment lib.
+      </h2>
     </section>
   );
 };
