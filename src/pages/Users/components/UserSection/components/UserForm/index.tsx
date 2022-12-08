2b570e515b4dbebbb9ba9138bfc18d480fcb1f8a
João Marcos <jmarcossferreira@hotmail.com>
2022-12-08 08:22:21 -0300

    Create userForm component

---
+++ src/pages/Users/components/UserSection/components/UserForm/index.tsx
@@ -0,0 +1,94 @@
+import React, { MutableRefObject, SyntheticEvent, useRef } from 'react';
+import { Button, Label, Select, TextInput } from 'flowbite-react';
+import { useUsersStore } from 'pages/Users/store';
+
+export const UserForm = () => {
+  const { profiles } = useUsersStore();
+
+  const nameRef = useRef<HTMLInputElement | null>(null);
+  const ageRef = useRef<HTMLInputElement | null>(null);
+  const loginRef = useRef<HTMLInputElement | null>(null);
+  const profileRef = useRef<HTMLSelectElement | null>(null);
+
+  const handleSubmit = (event: SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formValues = {
+      name: nameRef?.current?.value,
+      age: ageRef?.current?.value,
+      login: loginRef?.current?.value,
+    };
+
+    console.log(formValues);
+    resetForm();
+  };
+
+  const resetForm = () => {
+    nameRef.current!.value! = '';
+    ageRef.current!.value! = '';
+    loginRef.current!.value! = '';
+    profileRef.current!.value! = '';
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="bg-gray-50 p-8">
+      <Label htmlFor="name" value="Name" />
+
+      <TextInput
+        placeholder="Name"
+        id="name"
+        type="text"
+        required
+        ref={nameRef}
+      />
+
+      <div className="flex gap-4 w-full my-4">
+        <div className="w-full">
+          <Label htmlFor="age" value="Age" />
+          <TextInput
+            placeholder="age"
+            id="age"
+            type="number"
+            required
+            ref={ageRef}
+          />
+        </div>
+
+        <div className="w-full">
+          <Label htmlFor="login" value="Login" />
+          <TextInput
+            placeholder="Login"
+            id="login"
+            type="text"
+            required
+            ref={loginRef}
+          />
+        </div>
+      </div>
+
+      <div className="mb-4">
+        <Label htmlFor="profile" value="Profile" />
+        <Select
+          id="profile"
+          placeholder="Select a profile"
+          required
+          ref={profileRef}
+          defaultValue={''}
+        >
+          <option disabled value={''}>
+            Select a profile
+          </option>
+          {profiles.map(({ id, name }) => (
+            <option key={id} value={id}>
+              {name}
+            </option>
+          ))}
+        </Select>
+      </div>
+
+      <Button className="w-full" type="submit">
+        Save
+      </Button>
+    </form>
+  );
+};
